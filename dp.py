7e1f7e89845071b6a0e4a34126536dfcd6b767be
HopeWu <767088240@qq.com>
2022-07-16 16:57:48 +0800

    dp reduced version worked

--- dp.py
+++ dp.py
@@ -5,27 +5,7 @@ class Solution:
         self.count = len(coins)
         self.stack = []
         self.results = []
-        # self.memory = [0]*(1+target)
         self.memory = [[[] for j in coins] for i in range(1+target)]
-
-    def numberOfCombinations(self, coins: [int], target: int) -> int:
-        self.setup(coins, target)
-        n = target+1
-        self.target = target
-        # initializing
-        dp = [[0]*len(coins) for i in range(n)]
-        for index in range(len(coins)):
-            if coins[index] < target + 1:
-                dp[coins[index]][index] = 1
-        for i in range(1, target+1):
-            for index in range(len(coins)):
-                j = coins[index]
-                if i-j >= 0 and i-j <= target:
-                    if sum(dp[i-j]) > 0:
-                        dp[i][index] += sum(dp[i-j])
-        self.dp = dp
-        # self.printDp(target)
-        # return self.answer()
 
     def numberOfCombinations2(self, coins: [int], target: int) -> int:
         self.setup(coins, target)
@@ -45,42 +25,18 @@ class Solution:
             for index in range(len(coins)):
                 j = coins[index]
                 if i-j >= 0 and i-j <= target:
-                    if sum(dp[i-j]) > 0:
-                        dp[i][index] += sum(dp[i-j])
-
-                    for coin in self.memory[i-j]:
-                        for li in coin:
-                            if len(li) > 0:
-                                tmp = li.append(j)
-                                print(tmp)
-                                if len(tmp) > 0:
-                                    tmp.sort()
-                                    if tmp not in self.memory[i][index]:
-                                        self.memory[i][index].append(tmp)
+                    for coin in range(len(coins)):
+                        if dp[i-j][coin] > 0:
+                            dp[i][index] += dp[i-j][coin]
+                            for li in self.memory[i-j][coin]:
+                                # print(li)
+                                tmp = li[:]
+                                tmp.append(j)
+                                tmp.sort()
+                                if tmp not in self.memory[i][index]:
+                                    self.memory[i][index].append(tmp)
 
         self.dp = dp
-        # self.printDp(target)
-        # return self.answer()
-
-    def _printDp(self, target):
-        for row in self.dp:
-            print(row)
-
-    def printDp(self, target):
-        if target < 0:
-            return
-        if target == 0:
-            li = self.stack[:]
-            li.sort()
-            # print(li)
-            if li not in self.results:
-                self.results.append(li)
-            return
-        for i in range(self.count):
-            if self.dp[target][i] > 0:
-                self.stack.append(self.coins[i])
-                self.printDp(target-self.coins[i])
-                self.stack.pop()
 
     def printDp2(self, target):
         results = []
@@ -88,28 +44,8 @@ class Solution:
             for li in c:
                 if li not in results:
                     results.append(li)
-        return results
-
-
-    def countResults(self, target):
-        if target < 0:
-            return
-        if target == 0:
-            li = self.stack[:]
-            li.sort()
-            # print(li)
-            if li not in self.results:
-                self.results.append(li)
-            return
-        for i in range(self.count):
-            if self.dp[target][i] > 0:
-                self.stack.append(self.coins[i])
-                self.countResults(target-self.coins[i])
-                self.stack.pop()
-
-    def answer(self):
-        # print(len(self.results))
-        print(self.results)
+        print(results)
+        print(len(results))
 
 
 solution = Solution()
@@ -121,7 +57,7 @@ target = 28
 def start():
     solution.numberOfCombinations2(coins, target)
     solution.printDp2(target)
-    # solution.answer()
+
 
 start()
 
