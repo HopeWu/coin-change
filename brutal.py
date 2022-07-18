33e4d0234f4fe1a7234d9eda292f4762b6b606b4
HopeWu <767088240@qq.com>
2022-07-18 14:25:09 +0800

    brutal version complete

---
+++ brutal.py
@@ -0,0 +1,50 @@
+import time
+
+
+class Solution:
+    def setup(self, coins: [int], target):
+        import copy
+        self.coins = copy.deepcopy(coins)
+        self.target = target
+
+    def rcrsv(self, n: int, target: int):
+        if target == 0:
+            return 1
+
+        if n - 1 >= 0:
+            notTake = self.rcrsv(n-1, target)
+        else:
+            notTake = 0
+
+        if target - self.coins[n] >= 0:
+            take = self.rcrsv(n, target - self.coins[n])
+        else:
+            take = 0
+        return notTake + take
+
+    def numberOfCombinations(self, coins: [int], target: int) -> int:
+        self.setup(coins, target)
+        n = len(coins)
+        return self.rcrsv(n-1, target)
+
+
+def start():
+    solution = Solution()
+    coins = [1, 2, 4, 5]
+    target = 28
+    result = solution.numberOfCombinations(coins, target)
+    print(result)
+
+
+s = time.time()
+start()
+e = time.time()
+print("Time elapsed: ", e-s)
+
+
+'''
+solution.numberOfCombinations(coins, target)
+# solution._printDp(target)
+solution.printDp(target)
+solution.answer()
+'''
