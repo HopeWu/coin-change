07f7041c971af90542273d59e8d8cb8d325a8735
HopeWu <767088240@qq.com>
2022-07-18 11:26:45 +0800

    for comparing

--- a.cpp
+++ a.cpp
@@ -4,26 +4,18 @@ public:
         int n=coins.size();
         if(amount==0) return 1;
         
-        vector<int> prev(amount+1,0);
-        vector<int> curr(amount+1);
-        curr[0]=1;
+        vector<int> dp(amount+1,0);
+        dp[0]=1;
         
         for(int i=0;i<n;i++)
         {
-            for(int j=1;j<=amount;j++)
+            for(int j=coins[i];j<=amount;j++)
             {
-                int notTake=prev[j];
-                
-                int take;
-                if(coins[i]<=j) take=curr[j-coins[i]];
-                else take=0;
-                
-                curr[j]=take+notTake;
+                if(coins[i]<=j)
+                    dp[j]+=dp[j-coins[i]];
             }
-            prev=curr;
         }
-        
-        return prev[amount];
+        for(int &x:dp) cout<<x<<" ";
+        return dp[amount];
     }
 };
-
